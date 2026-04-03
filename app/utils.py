d4e3e25c5e1e0ba7e1ec8ea384b4d550b48ab15e
muzakkir2045 <muzakkir2045@gmail.com>
2026-04-03 15:39:54 +0530

    Fix utils and Rename title in landing page

--- app/utils.py
+++ app/utils.py
@@ -1,19 +1,14 @@
 import re
 
 def is_valid_username(username):
-
     if not username:
         return False, "Username cannot be empty."
-
     if len(username) < 4:
         return False, "Username must be at least 4 characters long."
-
     if len(username) > 20:
         return False, "Username cannot exceed 20 characters."
-
     if not re.fullmatch(r"[a-z0-9_]+", username):
         return False, "Username may contain only lowercase letters, numbers, and underscores."
-
     if username.startswith("_") or username.endswith("_"):
         return False, "Username cannot start or end with an underscore."
 
@@ -23,22 +18,17 @@ def is_valid_username(username):
     return True, None
 
 
-
 def is_strong_password(password):
-
     if len(password) < 8:
         return False, "Password must be at least 8 characters long."
-
     if not re.search(r"[A-Z]", password):
         return False, "Password must contain at least one uppercase letter."
-
     if not re.search(r"[a-z]", password):
         return False, "Password must contain at least one lowercase letter."
 
     if not re.search(r"\d", password):
         return False, "Password must contain at least one digit."
-
-    if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
+    if not re.search(r"[!@#$%^&*(),.?\":{}|<>_\-]", password):
         return False, "Password must contain at least one special character."
 
     return True, None
